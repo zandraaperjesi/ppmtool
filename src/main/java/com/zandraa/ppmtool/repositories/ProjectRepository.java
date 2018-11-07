34eeae9d56a3cef775d3d2f4d585d64d7a5e3754
Zandraa <zandraa.perjesi@gmail.com>
2018-11-07 22:36:22 +0100

    Add base entity and repo to project

---
+++ src/main/java/com/zandraa/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.zandraa.ppmtool.repositories;
+
+import com.zandraa.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+    
+}
